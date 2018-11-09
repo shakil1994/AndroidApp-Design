eff60521566f0be57fa73a3d5ca2780acbc0b071
android_shakil <shakhawathossen53@gmail.com>
2018-11-10 00:40:27 +0600

    Add new Model with Item

---
+++ FirebaseRCLMultipleType/app/src/main/java/com/example/shakil/firebaserclmultipletype/Model/Item.java
@@ -0,0 +1,40 @@
+package com.example.shakil.firebaserclmultipletype.Model;
+
+public class Item {
+    private String text;
+    private String subText;
+    private boolean expandable;
+
+    public Item() {
+    }
+
+    public Item(String text, String subText, boolean expandable) {
+        this.text = text;
+        this.subText = subText;
+        this.expandable = expandable;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSubText() {
+        return subText;
+    }
+
+    public void setSubText(String subText) {
+        this.subText = subText;
+    }
+
+    public boolean isExpandable() {
+        return expandable;
+    }
+
+    public void setExpandable(boolean expandable) {
+        this.expandable = expandable;
+    }
+}
